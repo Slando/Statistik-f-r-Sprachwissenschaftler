bcf0f3066f9914ef5d2939c9a4e7506e64c759ed
Slando <landow@students.uni-marburg.de>
2014-06-10 08:30:21 +0200

    Beginn HA 15

--- Landow/hausaufgabe15.R
+++ Landow/hausaufgabe15.R
@@ -1,6 +1,6 @@
 # Hausaufgabe 15
-# Phillip Alday <phillip.alday@staff.uni-marburg.de>
-# 2014-06-04
+# Landow, Steven <landow@students.uni-marburg.de>
+# 2014-06-10
 # Dieses Werk ist lizenziert unter einer CC-BY-NC-SA Lizenz.
 
 
